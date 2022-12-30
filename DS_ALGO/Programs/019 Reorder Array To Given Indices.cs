34ae3dbed1858e1f5083a3a4fa0b197850a09e17
abhi <53308336+abhinavkl@users.noreply.github.com>
2022-12-30 18:40:05 +0530

    Arrays medium programs

---
+++ DS_ALGO/Programs/019 Reorder Array To Given Indices.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_ALGO.Programs
+{
+    internal class _019_Reorder_Array_To_Given_Indices
+    {
+        int[] arr { get; set; }
+        int n { get { return arr.Length; } }
+        int[] indices { get; set; }
+        public _019_Reorder_Array_To_Given_Indices()
+        {
+            arr = new int[] { 50, 40, 70, 60, 90 };
+            indices = new int[n];
+            indices = new int[] { 3, 0, 4, 1, 2 };
+        }
+
+        public void Solve()
+        {
+            Reorder();
+        }
+
+        void Reorder()
+        {
+            for(int i = 0; i < n; i++)
+            {
+                if(indices[i] != i)
+                {
+                    int valueInActualIndex=arr[i];
+                    arr[i] = arr[indices[i]];
+                    arr[indices[i]] = valueInActualIndex;
+
+                    int indexInActualIndex = indices[indices[i]];
+                    indices[indices[i]] = indices[i];
+                    indices[i] = indexInActualIndex;
+
+                }
+            }
+        }
+
+    }
+}
---
+++ DS_ALGO/Programs/022 Merge Two Sorted Arrays.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_ALGO.Programs
+{
+    internal class _022_Merge_Two_Sorted_Arrays
+    {
+        int[] arr1;
+        int [] arr2;
+        int n1 { get { return arr1.Length; } }
+        int n2 { get { return arr2.Length; } }
+        public _022_Merge_Two_Sorted_Arrays()
+        {
+            arr1 = new int[] { 6, 8, 10, 11, 12, 15 };
+            arr2 = new int[] { 1, 3, 5, 7, 9, 13, 14 }; 
+        }
+
+        public void Solve()
+        {
+            if (n1 > n2)
+            {
+                MergeSwap(arr2, arr1);
+            }
+            else
+            {
+                MergeSwap(arr1,arr2);
+            }
+        }
+
+        void MergeSwap(int[] smallerArray, int[] largerArray)
+        {
+            int largerArrayPointer = 0;
+            int smallerArrayPointer = 0;
+
+            int largerArrayLength = largerArray.Length;
+            int smallerArrayLength=smallerArray.Length;
+
+            while(smallerArrayPointer<smallerArrayLength && largerArrayPointer < largerArrayLength)
+            {
+                int searchKey = smallerArray[smallerArrayPointer] > largerArray[largerArrayPointer] ? smallerArray[smallerArrayPointer] : largerArray[largerArrayPointer];
+                if (searchKey == smallerArray[smallerArrayPointer])
+                {
+                    smallerArrayPointer = NextGreaterElementIndex(largerArray, 0, largerArrayLength, searchKey);
+                }
+                else
+                {
+                    largerArrayPointer = NextGreaterElementIndex(smallerArray,0,smallerArrayPointer,searchKey);
+                }
+            }
+        }
+
+        int NextGreaterElementIndex(int[] searchArray,int searchFrom,int searchTo,int searchKey)
+        {
+            if (searchFrom<searchTo)
+            {
+                int mid = (searchTo - searchFrom) / 2 + searchFrom;
+
+                if (searchArray[mid] == searchKey)
+                    return mid;
+                else if (searchArray[mid] > searchKey)
+                    return NextGreaterElementIndex(searchArray, searchFrom, mid, searchKey);
+                else
+                    return NextGreaterElementIndex(searchArray, mid + 1, searchTo, searchKey);
+            }
+            return searchFrom;
+        }
+
+
+    }
+}
